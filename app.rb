90f8f3d80ef77097f25958f5527fd253f2065849
wlytle <wlytle89@gmail.com>
2020-10-25 20:59:30 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
+require "date"
+
 class App < Sinatra::Base
+  attr_accessor :date
+  get "/" do
+    erb :index
+  end
 
-	get '/' do
-		erb :index
-	end
+  get "/hello" do
+    erb :hello
+  end
 
+  get "/goodbye" do
+    erb :goodbye
+  end
 
+  get "/date" do
+    erb :date
+  end
 end
